e07032449209890c80533dd755664d8ec331f9b3
lorenzomorning <lorenzbeck11@gmail.com>
2021-09-05 13:53:33 +0200

    description of data

---
+++ R/world.cities.R
@@ -0,0 +1,15 @@
+#' World cities data
+#'
+#' A dataset containing world cities of population greater than about 40,000. Also included are capital cities of any population size, and many smaller towns.
+#'
+#' @format A data frame with 43645 rows and 6 variables (including latitude and longitude):
+#' \describe{
+#'   \item{name}{name, english}
+#'   \item{country.etc}{country name, english}
+#'   \item{pop}{population count}
+#'   \item{lat}{latitude in degree}
+#'   \item{long}{longitude in degree}
+#'   \item{capital}{binary}
+#' }
+#' @source \url{https://www.rdocumentation.org/packages/maps/versions/3.3.0/topics/world.cities}
+"world.cities"
